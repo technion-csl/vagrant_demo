dd2437ea2baed98bd18b026b49d7cd3562201dd7
Idan Yaniv <idanyani@cs.technion.ac.il>
2021-04-10 12:44:27 +0300

    Install docker on Ubuntu 18

---
+++ docker/install-on-ubuntu18.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+download_dir=/tmp
+docker_pool_url=https://download.docker.com/linux/ubuntu/dists/bionic/pool/stable/amd64
+cd $download_dir
+wget $docker_pool_url/containerd.io_1.4.4-1_amd64.deb
+wget $docker_pool_url/docker-ce-cli_20.10.5~3-0~ubuntu-bionic_amd64.deb
+wget $docker_pool_url/docker-ce_20.10.5~3-0~ubuntu-bionic_amd64.deb
+sudo dpkg -i containerd.io_1.4.4-1_amd64.deb
+sudo dpkg -i docker-ce-cli_20.10.5~3-0~ubuntu-bionic_amd64.deb
+sudo dpkg -i docker-ce_20.10.5~3-0~ubuntu-bionic_amd64.deb
+echo
+if [[ "$(groups)" == *"docker"* ]]; then
+    echo "The user $USER belongs to the docker group"
+else
+    echo "The user $USER does not belong to the docker group"
+    echo "Adding it via:"
+    sudo adduser $USER docker
+    echo "Please logout and login to belong to the new groups"
+    exit -1 # stop the script
+fi
+docker run hello-world
+
