4035f035904f59d2eda182cb7c2ddd83eac8a072
Idan Yaniv <idanyani@cs.technion.ac.il>
2021-08-11 16:48:28 +0300

    Update the linux kernel version 5.4.129 --> 5.4.139

--- linux/module.mk
+++ linux/module.mk
@@ -5,7 +5,7 @@ LINUX_BUILD_DIR := $(ROOT_DIR)/linux/build
 LINUX_INSTALL_DIR := $(ROOT_DIR)/linux/install
 # choose a specific linux kernel version with "cd linux && git checkout tags/v5.4"
 MAJOR_KERNEL_VERSION := 5.4
-MINOR_KERNEL_VERSION := 129
+MINOR_KERNEL_VERSION := 139
 KERNEL_VERSION := $(MAJOR_KERNEL_VERSION).$(MINOR_KERNEL_VERSION)
 # we can also extract the kernel version from the linux source tree via "cd linux && make kernelversion"
 # but this is problematic because $(LINUX_SOURCE_DIR) is empty right after "git clone"
